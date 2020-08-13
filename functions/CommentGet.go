c1f5c3a20187f997a1d67bcc38a4e2cc10663cfd
CSenshi <saba.pochkhua@gmail.com>
2020-08-13 19:25:21 +0400

    connect get with DB

--- functions/CommentGet.go
+++ functions/CommentGet.go
@@ -2,30 +2,44 @@
 package p
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"cloud.google.com/go/datastore"
 )
 
 // CommentGet function returns Comment with given id in json format
 func CommentGet(w http.ResponseWriter, r *http.Request) {
 	var d struct {
-		Message string `json:"message"`
+		ID int64 `json:"id"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Error While Parsing Request Body!")
+		_, _ = fmt.Fprint(w, "Error While Parsing Request Body!")
 		return
 	}
 
-	comment := Comment{
-		VoiceID: "123-456-789",
-		UserID:  "ab-cd-ef-gh",
-		Text:    "Best Voice Ever!!!"}
+	// 1. Connect to database
+	ctx := context.Background()
+	client, err := datastore.NewClient(ctx, ProjectName)
+	if err != nil {
+		fmt.Println(err) /* log error */
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// 2. Get data from database
+	var comment Comment
+	commentKey := datastore.NameKey(EntityName, strconv.FormatInt(d.ID, 10), nil)
+	err = client.Get(ctx, commentKey, &comment)
+	comment.ID = d.ID
 
 	byteArray, err := json.Marshal(comment)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Fprint(w, string(byteArray))
+	_, _ = fmt.Fprint(w, string(byteArray))
 }
