5206f183025f9ff1fcf142ff63f17d55f706897a
CSenshi <saba.pochkhua@gmail.com>
2020-08-13 19:38:02 +0400

    add logging

--- functions/CommentGet.go
+++ functions/CommentGet.go
@@ -33,12 +33,19 @@ func CommentGet(w http.ResponseWriter, r *http.Request) {
 	// 2. Get data from database
 	var comment Comment
 	commentKey := datastore.NameKey(EntityName, strconv.FormatInt(d.ID, 10), nil)
+	_, _ = fmt.Fprint(w, commentKey.String())
+	_, _ = fmt.Fprint(w, commentKey)
 	err = client.Get(ctx, commentKey, &comment)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	comment.ID = d.ID
 
 	byteArray, err := json.Marshal(comment)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	_, _ = fmt.Fprint(w, string(byteArray))
