df4109fe9f5ba8ec67a619bbd9b69058868560df
CSenshi <saba.pochkhua@gmail.com>
2020-08-14 14:10:03 +0400

    add generic deploy script

--- deploy-triggers.sh
+++ deploy-triggers.sh
@@ -1,2 +1,19 @@
-gcloud alpha builds triggers delete comment-get-trigger --quiet
-gcloud alpha builds triggers create github --trigger-config='workflows/trigger-configs/config-get.yaml'
+CONF_DIR='workflows/trigger-configs/'
+
+array=(
+  'comment-create-trigger':"${CONF_DIR}create.yaml"
+  'comment-get-trigger':"${CONF_DIR}get.yaml"
+  'comment-get-all-trigger':"${CONF_DIR}get-all.yaml"
+  'comment-update-trigger':"${CONF_DIR}update.yaml"
+)
+
+for i in "${array[@]}"; do IFS=":";
+  set -- ${i};
+
+  TRIGGER_NAME=${1}
+  TRIGGER_CONF=${2}
+  echo "#### Generating Trigger ${TRIGGER_NAME}"
+
+  gcloud alpha builds triggers delete "${TRIGGER_NAME}" --quiet
+  gcloud alpha builds triggers create github --trigger-config="${TRIGGER_CONF}"
+done
