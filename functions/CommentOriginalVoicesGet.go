857f852c2c4df9e469c3935dc14e1a7c978da225
CSenshi <saba.pochkhua@gmail.com>
2020-08-27 23:11:57 +0400

    add checking for parameers in url

--- functions/CommentOriginalVoicesGet.go
+++ functions/CommentOriginalVoicesGet.go
@@ -16,8 +16,11 @@ func CommentOriginalVoicesGet(w http.ResponseWriter, r *http.Request) {
 		ID string `json:"voiceId"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		_, _ = fmt.Fprint(w, "Error While Parsing Request Body!")
-		return
+		d.ID = r.FormValue("voiceId")
+		if d.ID == "" {
+			_, _ = fmt.Fprint(w, "Error While Parsing Request Body!\n URL: " + r.URL.Path)
+			return
+		}
 	}
 
 	// 2. Connect to database
